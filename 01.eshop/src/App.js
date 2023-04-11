25a6a8aa6508a90549cb6a76a4640921a8c0f1a7
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-04-11 23:27:19 +0900

    01.eShop completed...

--- 01.eshop/src/App.js
+++ 01.eshop/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+// import Login from './Login';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -10,6 +11,11 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          {/* <Route path="/login" element = {
+              <Login />
+            }
+          /> */}
+
           <Route path="/checkout" element = {
             <>
               <Header />
---
+++ 01.eshop/src/Firebase.js
@@ -0,0 +1,19 @@
+// import firebase from 'firebase';
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDVAulR-9jbg5Of-k9P1QbfqF7hs2nDzYg",
+//     authDomain: "eshop-a5031.firebaseapp.com",
+//     projectId: "eshop-a5031",
+//     storageBucket: "eshop-a5031.appspot.com",
+//     messagingSenderId: "303856757273",
+//     appId: "1:303856757273:web:12671ba75700e8697c1888",
+//     measurementId: "G-N960BDMD27"
+// };
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// const db = firebaseApp.firestore();
+
+// const auth = firebase.auth();
+
+// export { db, auth };
