f61bb2a2fb67b651ebb9e2b06fe37a9436a7e728
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-04-11 18:25:57 +0900

    props completed

--- 01.eshop/src/Product.js
+++ 01.eshop/src/Product.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './Product.css';
 
-const Product = () => {
+const Product = ({ id, title, image, price, rating }) => {
     return (
         <div className="product">
             <div className="product__info">
-                <p>Bennet Mystic 15.6 inch Laptop Sholuder Messaenger Sling Office Bag, Water
-                    Repellent Fabric
-                </p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>30</strong>
+                    <strong>{price}</strong>
                 </p>
-                <div className="product__rating">⭐⭐</div>
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>⭐</p>
+                        ))
+                    }
+                </div>
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="product-image" />
+            <img src={image}/>
 
             <button>Add to Basket</button>
         </div>
