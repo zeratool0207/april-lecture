fd579555b07cfacb7f21f9f7cf92f869880a235b
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-04-11 20:41:25 +0900

    eShop checkout Page completed ...

---
+++ 01.eshop/src/CheckoutProduct.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './CheckoutProduct.css';
+
+const CheckoutProduct = ({ id, image, title, price, rating }) => {
+    return (
+        <div className="checkoutProduct">
+            <img src={image} alt="" className="checkoutProduct__image" />
+
+            <div className="checkoutProduct__info">
+                <p className="checkoutProduct__title">
+                    {title}
+                </p>
+                <p className="checkoutProduct__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="checkoutProduct__rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <p>⭐</p>
+                    ))}
+                </div>
+                {/* <button onClick={removeFromBasket}>Remove from Basket</button> */}
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutProduct
