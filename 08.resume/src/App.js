44349fd43b11f503404ab35d9fbe277f74fe67ca
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-04-21 07:03:32 +0900

    resume proceeding...

--- 08.resume/src/App.js
+++ 08.resume/src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Skills from './components/pages/skills/Skills';
+import Experiences from './components/pages/experiences/Experiences';
+import Educations from './components/pages/educations/Educations';
+import Portfolios from './components/pages/portfolios/Portfolios';
 import './App.css';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/skills', element: <Skills /> },
+  { path: '/experiences', element: <Experiences /> },
+  { path: '/educations', element: <Educations /> },
+  { path: '/portfolios', element: <Portfolios /> },
+
+])
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to resume website</h1>
-    </div>
+    <RouterProvider router = {router}/>
+    // <Router>
+    //   <>
+    //     <Route exact path="/" component={Home} />
+    //     <Switch>
+    //       <Route path="/skills" component={Skills} />
+    //       <Route path="/experiences" component={Experiences} />
+    //       <Route path="/educations" component={Educations} />
+    //       <Route path="/portfolios" component={Portfolios} />
+    //     </Switch>
+    //   </>
+    // </Router>
   );
 }
 
