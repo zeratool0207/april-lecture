9c7f268d74c9f86fe28dd08a34291e7bdb06564b
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-04-24 07:35:12 +0900

    99.redux-book-17 add

---
+++ 99.redux-book-17/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div>
+      <Counter number={0} />
+      <hr />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
