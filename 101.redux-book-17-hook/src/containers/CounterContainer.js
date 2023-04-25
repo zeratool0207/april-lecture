90b67593f6e5f91e225ff486bc4e650366b61747
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-04-26 06:51:02 +0900

    redux-book add

--- 101.redux-book-17-hook/src/containers/CounterContainer.js
+++ 101.redux-book-17-hook/src/containers/CounterContainer.js
@@ -1,76 +1,20 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Counter from '../components/Counter';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { increase, decrease } from '../modules/counter'; 
-import {bindActionCreators} from 'redux';
 
-const CounterContainer = ({ number, increase, decrease}) => {
-    return (
-        <Counter 
-            number = {number}
-            onIncrease={increase}
-            onDecrease = {decrease}
-        />
-    )
+const CounterContainer = () => {
+   const number = useSelector(state => state.counter.number);
+   const dispatch = useDispatch();
+   const onIncrease = useCallback( () => dispatch(increase()), [dispatch]);
+   const onDecrease = useCallback( () => dispatch(decrease()), [dispatch]);
+   return (
+    <Counter
+        number={number}
+        onIncrease={onIncrease}
+        onDecrease={onDecrease}
+    />
+   )
 }
 
-// 방법4
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    {
-        increase,
-        decrease
-    },
-)(CounterContainer)
-
-
-// 방법3
-// export default connect(
-//     state => ({
-//         number: state.counter.number,
-//     }),
-//     dispatch => 
-//         bindActionCreators(
-//             {
-//                 increase,
-//                 decrease,
-//             },
-//             dispatch,
-//         ),
-// )(CounterContainer);
-
-
-// 방법2
-// export default connect(
-//     state => ({
-//         number: state.counter.number,
-//     }),
-//     dispatch => ({
-//         increase : () => dispatch(increase()),
-//         decrease : () => dispatch(decrease()),
-//     }),
-// )(CounterContainer);
-
-
-
-// 방법1
-// const mapStateToProps = state => ({
-//     number: state.counter.number
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//     increase: () => {
-//         dispatch(increase());
-//     },
-//     decrease: () => {
-//         dispatch(decrease());
-//     },
-
-// })
-
-// export default connect(
-//     mapStateToProps,
-//     mapDispatchToProps,
-// )(CounterContainer);
+export default CounterContainer;
